c48ecfe735692e8e25b10578b56e8060930e4d88
samirls <samirlaguardiaii@gmail.com>
2023-03-31 23:21:57 -0300

    installed CORS to deal with Access Control Allow Origin

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"
 import connectDatabase from "./src/database/database.js";
 import dotenv from "dotenv";
 
@@ -14,6 +15,7 @@ const app = express();
 
 connectDatabase();
 app.use(express.json());
+app.use(cors());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
